e617c36d814953909a6c88b132e2fd6611617d8c
Mitchell Lee <mitchell.t.lee@gmail.com>
2018-10-14 15:12:52 -0700

    Created hello-world src and tests

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route('/hello_world')
+def hello_world():
+    return jsonify({'hello': 'world'})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ tests/main_test.py
@@ -0,0 +1,14 @@
+from main import app
+import json
+import unittest
+
+
+class MainTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_hello_world(self):
+        response = self.app.get("/hello_world")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), {"hello": "world"})
