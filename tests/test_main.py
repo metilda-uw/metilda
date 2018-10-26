0a5ef5dd640e041d3bece0d1d3853f26796403ef
Mitchell Lee <mitchell.t.lee@gmail.com>
2018-10-25 19:40:26 -0700

    [#2] Updated tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,8 +7,3 @@ class MainTest(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-
-    def test_hello_world(self):
-        response = self.app.get("/api/hello_world")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.data), {"hello": "world"})
