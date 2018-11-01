234980cfd8d97c7bd1c96a10c705304c5f4fa099
Mitchell Lee <mitchl2@bitbucket.org>
2018-10-31 21:27:01 -0700

    [#2] Implemented python backend for step 1

---
+++ src/controllers/pitch_art_wizard.py
@@ -0,0 +1,30 @@
+import os
+
+import json
+from flask import request, jsonify, send_from_directory
+from werkzeug.utils import secure_filename
+
+from main import app
+
+
+@app.route('/api/upload-audio-file', methods=["POST"])
+def upload_audio_file():
+    file = request.files["file"]
+    file_name = secure_filename(file.filename)
+    path = os.path.join(app.config["UPLOAD_FOLDER"], file_name)
+    file.save(path)
+    return jsonify({"id": file_name.split(".")[0]})
+
+
+@app.route('/api/upload-audio-metadata', methods=["POST", "GET"])
+def upload_audio_metadata():
+    upload_json = request.get_json()
+    file_name = secure_filename(upload_json["id"] + ".json")
+    path = os.path.join(app.config["UPLOAD_FOLDER"], file_name)
+    if request.method == "POST":
+        with open(path, "w") as fh:
+            json.dump(upload_json, fh)
+            return "", 200
+    else:
+        with open(path, "r") as fh:
+            return jsonify(json.load(fh))
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 app = Flask(__name__,
             static_folder="../frontend/build/static",
             template_folder="../frontend/build")
-import pitch_art_wizard_api
 
+from controllers import pitch_art_wizard
 
 @app.route('/')
 def homepage():
