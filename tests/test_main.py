1efa712bdc9a832c88f9df7fe34e622a60caafbd
Mitchell Lee <mitchell.t.lee@gmail.com>
2018-11-01 22:15:02 -0700

    [#1] Fixed tests not running

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from main import app
-import json
-import unittest
-
-
-class MainTest(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
