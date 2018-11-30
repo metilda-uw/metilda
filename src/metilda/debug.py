43e0ac3025169f322ff7c6be36c4e1a96d695eae
Mitchell Lee <mitchell.t.lee@gmail.com>
2018-11-29 19:55:11 -0800

    [#6] Added warning message

--- src/metilda/debug.py
+++ src/metilda/debug.py
@@ -1,3 +1,14 @@
+################
+#
+#   There is a known issue with using flask's built-in server (i.e. this script)
+#   and serving files to the user. See https://stackoverflow.com/q/37962925 for
+#   more information.
+#
+#   When testing the site's audio functionality, you must instead launch the app
+#   with a production server. See runLocalServer.sh
+#
+################
+
 # Launcher for local, debug runs
 from metilda import get_app
 
--- src/metilda/main.py
+++ src/metilda/main.py
@@ -1,3 +1,15 @@
+################
+#
+#   There is a known issue with using flask's built-in server (i.e. this script)
+#   and serving files to the user. See https://stackoverflow.com/q/37962925 for
+#   more information.
+#
+#   When testing the site's audio functionality, you must instead launch the app
+#   with a production server. See runLocalServer.sh
+#
+################
+
+
 from metilda import get_app
 
 if __name__ == "__main__":
