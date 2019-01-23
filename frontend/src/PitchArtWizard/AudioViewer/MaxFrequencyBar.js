91f3473912a715fcd0fc17ea0fa46d844983de04
Mitchell Lee <mitchell.t.lee@gmail.com>
2019-01-22 20:42:15 -0800

    [#35] Setup validation for pitch ceiling input

--- frontend/src/PitchArtWizard/AudioViewer/MaxFrequencyBar.js
+++ frontend/src/PitchArtWizard/AudioViewer/MaxFrequencyBar.js
@@ -6,25 +6,37 @@ import {controls, Media, Player} from 'react-media-player';
 const {PlayPause, MuteUnmute, SeekBar} = controls;
 
 class MaxFrequencyBar extends Component {
+    constructor(props) {
+        super(props);
+        this.submitMaxPitch = this.submitMaxPitch.bind(this);
+    }
+
+    submitMaxPitch(event) {
+        event.preventDefault();
+        this.props.applyMaxPitch();
+    }
+
     render() {
         return (
             <div className="col s12">
                 <div className="row">
-                    <div className="input-field col s9">
-                        <input name="maxPitch"
-                               id="maxPitch"
-                               onChange={this.props.handleInputChange}
-                               placeholder="hertz"
-                               className="validate"
-                               pattern="(\d+)?(\.\d+)?"
-                               type="text"/>
-                        <label className="active" htmlFor="maxPitch">Pitch Ceiling</label>
-                        <span className="helper-text" data-error="Must be a positive number"></span>
-                    </div>
-                    <div className="input-field col s2">
-                        <button className="waves-effect waves-light btn"
-                           onClick={this.props.applyMaxPitch}>Apply</button>
-                    </div>
+                    <form onSubmit={this.submitMaxPitch}>
+                        <div className="input-field col s9">
+                            <input name="maxPitch"
+                                   id="maxPitch"
+                                   onChange={this.props.handleInputChange}
+                                   placeholder="hertz"
+                                   className="validate"
+                                   pattern="(\d+)?(\.\d+)?"
+                                   type="text"/>
+                            <label className="active" htmlFor="maxPitch">Pitch Ceiling</label>
+                        </div>
+                        <div className="input-field col s2">
+                            <button className="waves-effect waves-light btn"
+                                    type="submit">Apply
+                            </button>
+                        </div>
+                    </form>
                 </div>
 
 
