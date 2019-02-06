b35260d0c9571efe460498eed8340d1accd78f6b
Mitchell Lee <mitchell.t.lee@gmail.com>
2019-02-05 20:06:14 -0800

    [#54] Added default text.

--- frontend/src/PitchArtWizard/AudioImgDefault.js
+++ frontend/src/PitchArtWizard/AudioImgDefault.js
@@ -11,7 +11,7 @@ class AudioImgDefault extends React.Component {
     render() {
         return (
             <div className="metilda-audio-analysis-image-default">
-
+                <p>Choose an audio file to begin</p>
             </div>
         )
     }
