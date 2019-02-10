5eda6d1115f59d6de1a5015cfe308a647826105c
Mitchell Lee <mitchell.t.lee@gmail.com>
2019-02-09 19:14:20 -0800

    [#57] Added tests

--- frontend/src/PitchArtWizard/PitchArtViewer/PitchArtScale.js
+++ frontend/src/PitchArtWizard/PitchArtViewer/PitchArtScale.js
@@ -1,7 +1,3 @@
-exports.sum = function(x, y) {
-    return x + y;
-};
-
 exports.roundToNearestNote = function(pitch, scaleDivisions=8.0) {
     // Scale divisions determines how the scale is divided. Examples:
     // - 2 makes all notes round to the nearest whole tone
@@ -11,6 +7,14 @@ exports.roundToNearestNote = function(pitch, scaleDivisions=8.0) {
     let scaleBase = Math.pow(2, 1 / (12.0 * scaleDivisions / 2.0));
     let nearestTonePitchExp = Math.round(Math.log(pitch / referenceNote) / Math.log(scaleBase));
     return referenceNote * Math.pow(scaleBase, nearestTonePitchExp);
-}
+};
 
-
+exports.referenceExponent = function(pitch, scaleDivisions=8.0) {
+    // Scale divisions determines how the scale is divided. Examples:
+    // - 2 makes all notes round to the nearest whole tone
+    // - 4 makes all notes round to the nearest quarter tone
+    // - 8 makes all notes round to the nearest eighth tone
+    let referenceNote = 440.0;
+    let scaleBase = Math.pow(2, 1 / (12.0 * scaleDivisions / 2.0));
+    return Math.round(Math.log(pitch / referenceNote) / Math.log(scaleBase));
+};
--- frontend/src/PitchArtWizard/PitchArtViewer/PitchArtScale.test.js
+++ frontend/src/PitchArtWizard/PitchArtViewer/PitchArtScale.test.js
@@ -1,4 +1,5 @@
-import {roundToNearestNote} from "./PitchArtScale";
+import {roundToNearestNote,
+        referenceExponent} from "./PitchArtScale";
 
 it("roundToNearestNote whole-tone scale", () => {
    expect(roundToNearestNote(439.0, 2)).toEqual(440.0);
@@ -7,4 +8,16 @@ it("roundToNearestNote whole-tone scale", () => {
 it("roundToNearestNote eighth-tone scale", () => {
     expect(roundToNearestNote(439.0, 8)).toEqual(440.0);
     expect(roundToNearestNote(428, 8)).toBeCloseTo(427.47, 2);
+});
+
+it("referenceExponent whole-tone scale", () => {
+    expect(referenceExponent(392.00, 2)).toEqual(-2);
+    expect(referenceExponent(440.0, 2)).toEqual(0);
+    expect(referenceExponent(493.88, 2)).toEqual(2);
+});
+
+it("referenceExponent eighth-tone scale", () => {
+    expect(referenceExponent(392.00, 8)).toEqual(-8);
+    expect(referenceExponent(440.0, 8)).toEqual(0);
+    expect(referenceExponent(493.88, 8)).toEqual(8);
 });
