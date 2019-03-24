1c1170d0fbe37ea71cfd478769e0df6fc155521c
Mitchell Lee <mitchell.t.lee@gmail.com>
2019-03-24 11:25:23 -0700

    [#72] Implemented the ability to show pitch art image selections.

--- frontend/src/PitchArtWizard/learn/WordSyllableCategories.tsx
+++ frontend/src/PitchArtWizard/learn/WordSyllableCategories.tsx
@@ -1,6 +1,14 @@
 import * as React from "react";
 import "./WordSyllableCategories.css";
 import {ChangeEvent, SyntheticEvent} from "react";
+import pitchArt21 from "./images/Pitch Art - 21-01.jpg";
+import pitchArt22 from "./images/Pitch Art - 22-01.jpg";
+import pitchArt31 from "./images/Pitch Art - 31-01.jpg";
+import pitchArt32 from "./images/Pitch Art - 32-01.jpg";
+import pitchArt33 from "./images/Pitch Art - 33-01.jpg";
+import pitchArt41 from "./images/Pitch Art - 41-01.jpg";
+import pitchArt42 from "./images/Pitch Art - 42-01.jpg";
+import pitchArt43 from "./images/Pitch Art - 43-01.jpg";
 
 interface Props {
 
@@ -22,6 +30,19 @@ export default class WordSyllableCategories extends React.Component<Props, State
         let name = event.currentTarget.name;
         let value = event.currentTarget.value;
         this.setState({[name]: parseFloat(value)} as any);
+    };
+
+    imageSrcList = () => {
+        switch (this.state.numSyllables) {
+            case 2:
+                return [pitchArt21, pitchArt22];
+            case 3:
+                return [pitchArt31, pitchArt32, pitchArt33];
+            case 4:
+                return [pitchArt41, pitchArt42, pitchArt43];
+            default:
+                return [];
+        }
     };
 
     render() {
@@ -69,6 +90,17 @@ export default class WordSyllableCategories extends React.Component<Props, State
                                         </label>
                                     </p>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col s6">
+                            <div className="col s12 pitch-art-img-list">
+                                { this.imageSrcList().map((item, index) =>
+                                    <img src={item}
+                                         key={"pitch-art-img-list-item-" + index}
+                                         className="pitch-art-img-list-item"/>
+                                )}
                             </div>
                         </div>
                     </div>
--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,3 +1,7 @@
 /// <reference types="react-scripts" />
 declare module 'react-media-player';
 declare module 'recorder-js';
+declare module "*.jpg" {
+  const content: any;
+  export default content;
+}
