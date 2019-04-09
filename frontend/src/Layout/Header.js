17d76979f531b2d94073c26733034eaa57ff934b
Mitchell Lee <mitchell.t.lee@gmail.com>
2019-04-08 20:26:46 -0700

    [#65] Changed header styling.

--- frontend/src/Layout/Header.js
+++ frontend/src/Layout/Header.js
@@ -6,21 +6,38 @@ import {Link} from "react-router-dom";
 class Header extends Component {
     state = {};
 
-  componentDidMount() {}
+    componentDidMount() {
+    }
 
-  render() {
-    return (
-        <nav>
-            <div id="metilda-navbar" className="row">
-                <a href="/pitchartwizard" className="app-logo metilda-navbar-item">MeTILDA</a>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="/pitchartwizard">Create</a></li>
-                    <li><a href="/learn/words/syllables">Learn</a></li>
-                </ul>
-            </div>
-        </nav>
-    );
-  }
+    renderNavBarItem = (title, link, index) => {
+        return (
+            <li key={index} className={window.location.href.includes(link) ? 'active' : ''}>
+                <a href={link}>
+                    {title}
+                </a>
+            </li>
+        );
+    }
+
+    render() {
+        const navBarLinks = [
+            {title: "Create", link: "/pitchartwizard"},
+            {title: "Learn", link: "/learn/words/syllables"}
+        ];
+
+        return (
+            <nav>
+                <div id="metilda-navbar" className="row">
+                    <a href="/pitchartwizard" className="app-logo metilda-navbar-item">MeTILDA</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        {navBarLinks.map((item, index) =>
+                            this.renderNavBarItem(item.title, item.link, index)
+                        )}
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
 }
 
 export default Header;
