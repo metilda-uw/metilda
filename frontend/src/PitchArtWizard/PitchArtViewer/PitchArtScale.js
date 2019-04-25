4dc4f693f8a72458474da47ea7bb03d6d9a7e1b3
Mitchell Lee <mitchell.t.lee@gmail.com>
2019-04-24 19:36:16 -0700

    Refactored functions for computing perceptual scale values

--- frontend/src/PitchArtWizard/PitchArtViewer/PitchArtScale.js
+++ frontend/src/PitchArtWizard/PitchArtViewer/PitchArtScale.js
@@ -1,30 +1,23 @@
+const REFERENCE_NOTE = 440.0;
+
 export const roundToNearestNote = function(pitch, scaleDivisions=8.0) {
-    // Scale divisions determines how the scale is divided. Examples:
-    // - 2 makes all notes round to the nearest whole tone
-    // - 4 makes all notes round to the nearest quarter tone
-    // - 8 makes all notes round to the nearest eighth tone
-    let referenceNote = 440.0;
-    let scaleBase = Math.pow(2, 1 / (12.0 * scaleDivisions / 2.0));
-    let nearestTonePitchExp = Math.round(Math.log(pitch / referenceNote) / Math.log(scaleBase));
-    return referenceNote * Math.pow(scaleBase, nearestTonePitchExp);
+    let nearestTonePitchExp = referenceExponent(pitch, scaleDivisions);
+    return exponentToNote(nearestTonePitchExp, scaleDivisions);
 };
 
 export const referenceExponent = function(pitch, scaleDivisions=8.0) {
-    // Scale divisions determines how the scale is divided. Examples:
-    // - 2 makes all notes round to the nearest whole tone
-    // - 4 makes all notes round to the nearest quarter tone
-    // - 8 makes all notes round to the nearest eighth tone
-    let referenceNote = 440.0;
-    let scaleBase = Math.pow(2, 1 / (12.0 * scaleDivisions / 2.0));
-    return Math.round(Math.log(pitch / referenceNote) / Math.log(scaleBase));
+    return Math.round(exponentScalar(scaleDivisions) * Math.log2(pitch / REFERENCE_NOTE));
 };
 
 export const exponentToNote = function(exponent, scaleDivisions=8.0) {
+    let scaleBase = Math.pow(2, 1 / exponentScalar(scaleDivisions));
+    return REFERENCE_NOTE * Math.pow(scaleBase, exponent);
+};
+
+const exponentScalar = function(scaleDivisions) {
     // Scale divisions determines how the scale is divided. Examples:
     // - 2 makes all notes round to the nearest whole tone
     // - 4 makes all notes round to the nearest quarter tone
     // - 8 makes all notes round to the nearest eighth tone
-    let referenceNote = 440.0;
-    let scaleBase = Math.pow(2, 1 / (12.0 * scaleDivisions / 2.0));
-    return referenceNote * Math.pow(scaleBase, exponent);
+    return 12.0 * scaleDivisions / 2.0;
 };
