c739c3e43ce6200209a6109de58b5b0601073b7f
Mitchell Lee <mitchell.t.lee@gmail.com>
2019-06-03 19:20:43 -0700

    Changed server launcher to use python script

--- runLocalServer.sh
+++ runLocalServer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-PYTHONPATH="$PYTHONPATH;/venv/Lib;/venv/Lib/site-packages;/venv/Scripts;src" ./venv/Scripts/waitress-serve.exe --port=5000 --call metilda:get_app
+PYTHONPATH=src/ ./venv/Scripts/python ./src/metilda/local_server.py
