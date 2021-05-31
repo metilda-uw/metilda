c7fde7760a0dd6552b90f4ddda0b60f0eafbde14
Utsav Goswami <utsavg19@googlemail.com>
2021-05-31 00:49:34 -0700

    incorporated RESTful API calls for fetching available words by syllable

--- frontend/src/Learn/WordSyllableCategories.tsx
+++ frontend/src/Learn/WordSyllableCategories.tsx
@@ -19,6 +19,8 @@ import wordSyllableService from "./services/WordSyllables";
 interface State {
     viewStudentsClicked: boolean;
     isTeacher: boolean;
+    imageSrcList: string[];
+    imagesLoaded: boolean;
 }
 interface Props extends RouteComponentProps {
     firebase: any;
@@ -30,7 +32,9 @@ class WordSyllableCategories extends React.Component<Props, State> {
 
         this.state = {
             viewStudentsClicked: false,
-            isTeacher: false
+            isTeacher: false,
+            imageSrcList: [],
+            imagesLoaded: false
         };
       }
 
@@ -52,11 +56,26 @@ class WordSyllableCategories extends React.Component<Props, State> {
                 });
               }
           });
-      }
+
+        const initNumSyllables: number = this.getNumSyllables();
+
+        if (!isNaN(initNumSyllables)) {
+            wordSyllableService.getWordsBySyllableCount(initNumSyllables)
+                .then((res) => {
+                    this.setState({
+                        imageSrcList: res.data.map((word: any) => word.imagePath)
+                                              .filter((word: any) => word !== null),
+                        imagesLoaded: true
+                    });
+                });
+        }
+
+    }
 
     handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         const name = event.currentTarget.name;
         const value = event.currentTarget.value;
+
         this.setState({[name]: parseFloat(value)} as any);
     }
 
@@ -66,7 +85,7 @@ class WordSyllableCategories extends React.Component<Props, State> {
 
         if (!numSyllables) {
             if (this.props.location.pathname === "/learn/words/syllables") {
-                return 2;
+                return NaN;
             } else {
                 return parseFloat(this.props.location.pathname.slice(-1));
             }
@@ -77,11 +96,6 @@ class WordSyllableCategories extends React.Component<Props, State> {
 
     imageSrcList = () => {
         const numSyllables: number = this.getNumSyllables();
-
-        wordSyllableService.getWordsBySyllableCount(numSyllables)
-            .then(res => console.log(res.data));
-
-        const bruh = "./images/Pitch Art - 21-01.jpg";
 
         switch (this.getNumSyllables()) {
             case 2:
@@ -104,8 +118,7 @@ class WordSyllableCategories extends React.Component<Props, State> {
         if (this.props.location.pathname === "/learn/words/syllables") { // WordSyllableCategories component
             if (nextProps.location.search.slice(0, -1) === "?accentIndex=") { // when user clicks picth art image
                 return false;  // when pitch art is selected, stop re-render WordSyllableCategories component
-            } else if (numSyllables !== numSyllablesNext) { 
-                // when user clicks different "Number of syllables" radio button
+            } else if (numSyllables !== numSyllablesNext) {
                 return true;   // re-render pitch art images
             }
         }
@@ -117,6 +130,31 @@ class WordSyllableCategories extends React.Component<Props, State> {
         }
         this.resetSize();                   
         return true;
+    }
+
+    componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>, snapshot?: any) {
+
+        const currentValues: queryString.ParsedQuery<string> = queryString.parse(this.props.location.search);
+        const currentNumSyllables: number = parseFloat(currentValues.numSyllables as string);
+
+        const prevValues: queryString.ParsedQuery<string> = queryString.parse(prevProps.location.search);
+        const prevNumSyllables: number = parseFloat(prevValues.numSyllables as string);
+
+        if (currentNumSyllables !== prevNumSyllables) {
+
+            if (this.state.imagesLoaded) {
+                this.setState({ imagesLoaded: false });
+            }
+
+            wordSyllableService.getWordsBySyllableCount(currentNumSyllables)
+                .then((res) => {
+                    this.setState({
+                        imageSrcList: res.data.map((word: any) => word.imagePath)
+                            .filter((word: any) => word !== null),
+                        imagesLoaded: true
+                    });
+                });
+        }
     }
 
     displayStudents = async () => {
@@ -132,8 +170,16 @@ class WordSyllableCategories extends React.Component<Props, State> {
       }
     
     resetSize = () => {
-        for (let i = 0; i < this.imageSrcList().length; i++) {
-            document.getElementById("pitchArt" + this.getNumSyllables() + i)!.className = "pitch-art-img-list-item-smaller";
+        if (this.state.imagesLoaded) {
+            for (let i = 0; i < this.imageSrcList().length; i++) {
+
+                const pitchArtElement: HTMLElement = document.getElementById("pitchArt" + this.getNumSyllables() + i)!;
+
+                if (pitchArtElement) {
+                    pitchArtElement.className = "pitch-art-img-list-item-smaller";
+                }
+
+            }
         }
     }
 
@@ -142,8 +188,43 @@ class WordSyllableCategories extends React.Component<Props, State> {
         document.getElementById("pitchArt" + numSyllables + index)!.className = "pitch-art-img-list-item";
     }
 
+    onChangeValue(event: any) {
+        console.log(event.target.value);
+    }
+
     render() {
-        const numSyllables = this.getNumSyllables();
+        const numSyllables: number = this.getNumSyllables();
+
+        let imageContent;
+
+        if (this.state.imagesLoaded) {
+            imageContent = <div className="row">
+                        <div className="col s6">
+                            <div className="col s12 pitch-art-img-list">
+                                {this.state.imageSrcList.map((item, index) => {
+                                        const photo = require("." + item);
+
+                                        return <Link key={index}
+                                              to={"/learn/words/syllables/" + numSyllables + "?accentIndex=" + index}>
+                                            <img src={photo}
+                                                 key={"pitch-art-img-list-item-" + index}
+                                                 className="pitch-art-img-list-item-smaller"
+                                                 id={"pitchArt" + numSyllables + index}
+                                                 onClick={() => this.enlargeSize
+                                                 (numSyllables, index, this.state.imageSrcList.length)}
+                                            />
+                                        </Link>;
+                                    }
+                                )}
+                            </div>
+                        </div>
+                    </div>;
+        } else if (isNaN(numSyllables)) {
+            imageContent = <div>Pick a Syllable Number...</div>;
+        } else {
+            imageContent = <div>Loading...</div>;
+        }
+
         return (
             <div>
                 <Header/>
@@ -155,70 +236,50 @@ class WordSyllableCategories extends React.Component<Props, State> {
                         Blackfoot Words
                     </h5>
                 </div>
-                <div className="metilda-page-content">
+                <div className="metilda-page-content" >
                     <div className="row syllable-selection-row">
                         <div className="col s4">
                             <div id="syllable-selection" className="col s12">
                                 <p className="inline-btns-label">Number of syllables</p>
-                                <div className="inline-btns">
+                                <form className="inline-btns">
                                     <Link to="/learn/words/syllables?numSyllables=2">
                                         <p>
-                                            <label>
+                                            <div onChange={(evt) => this.onChangeValue(evt)}>
                                                 <input name="numSyllables"
                                                        type="radio"
                                                        value="2"
-                                                       onChange={this.handleInputChange}
                                                        checked={numSyllables === 2}/>
                                                 <span>2</span>
-                                            </label>
+                                            </div>
                                         </p>
                                     </Link>
                                     <Link to="/learn/words/syllables?numSyllables=3">
                                         <p>
-                                            <label>
+                                            <div onChange={(evt) => this.onChangeValue(evt)}>
                                                 <input name="numSyllables"
                                                        type="radio"
                                                        value="3"
-                                                       onChange={this.handleInputChange}
                                                        checked={numSyllables === 3}/>
                                                 <span>3</span>
-                                            </label>
+                                            </div>
                                         </p>
                                     </Link>
                                     <Link to="/learn/words/syllables?numSyllables=4">
                                         <p>
-                                            <label>
+                                            <div onChange={(evt) => this.onChangeValue(evt)}>
                                                 <input name="numSyllables"
                                                        type="radio"
                                                        value="4"
-                                                       onChange={this.handleInputChange}
                                                        checked={numSyllables === 4}/>
                                                 <span>4</span>
-                                            </label>
+                                            </div>
                                         </p>
                                     </Link>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col s6">
-                            <div className="col s12 pitch-art-img-list">
-                                {this.imageSrcList().map((item, index) =>
-                                    <Link key={index}
-                                    to={"/learn/words/syllables/" + numSyllables + "?accentIndex=" + index}>
-                                        <img src={item}
-                                             key={"pitch-art-img-list-item-" + index}
-                                             className="pitch-art-img-list-item-smaller"
-                                             id = {"pitchArt" + numSyllables + index}
-                                             onClick={() => this.enlargeSize
-                                             (numSyllables, index, this.imageSrcList().length)}
-                                        />
-                                    </Link>
-                                )}
-                            </div>
-                        </div>
-                    </div>
+                    {imageContent}
                 </div>
             <StudentsInfo showStudentsInfo={this.state.viewStudentsClicked}
             studentsInfoBackButtonClicked={this.studentsInfoBackButtonClicked}/>
