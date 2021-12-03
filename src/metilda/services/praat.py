80c333ba62d7aea29a3d48f0629e33938999415f
ChrisLee351 <chris.lee.351@hotmail.com>
2021-12-03 18:36:19 +0000

    Removing Flask implementation from praat

--- src/metilda/services/praat.py
+++ src/metilda/services/praat.py
@@ -1,15 +1,22 @@
 from __future__ import absolute_import
 import subprocess
-from flask import Flask
-from flask_cors import CORS
+#from flask import Flask
+#from flask_cors import CORS
+from os import environ
 
-# Locations of required files
-#TODO: This path needs to change based on whether you are working locally or deploying to Heroku
-_images_dir = "src/metilda/images/"
-_scripts_dir = "src/metilda/scripts/"
-_sounds_dir = "sounds/"
-_eaf_dir = "src/metilda/eaf/"
-_linkElanPraat_dir = "combined/"
+# Locations of required files - path needs to change based on whether you are working locally or deploying to Heroku
+if environ.get('FLASK_ENV') == "development":
+   _images_dir = "images/"
+   _scripts_dir = "metilda/scripts/"
+   _sounds_dir = "sounds/"
+   _eaf_dir = "metilda/eaf/"
+   _linkElanPraat_dir = "combined/"   
+else:
+   _images_dir = "src/metilda/images/"
+   _scripts_dir = "src/metilda/scripts/"
+   _sounds_dir = "sounds/"
+   _eaf_dir = "src/metilda/eaf/"
+   _linkElanPraat_dir = "combined/"
 
 # Run script 'scriptName' with the provided parameters
 def runScript(scriptName, args):
@@ -22,9 +29,9 @@ def runScript(scriptName, args):
    return output.decode("utf-8")
 
 # Create flask app
-app = Flask(__name__, static_url_path="")
+#app = Flask(__name__, static_url_path="")
 
 # Add CORS headers to allow cross-origin requests
-CORS(app)
+#CORS(app)
 
 #Import views
