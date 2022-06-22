6a52dd80f5d70834e5402a8f8c4ad1141257e04e
ChrisLee351 <chris.lee.351@hotmail.com>
2022-06-22 23:25:17 +0000

    Add Speaker Name to Redux

--- frontend/src/types/types.tsx
+++ frontend/src/types/types.tsx
@@ -1,17 +1,18 @@
 export interface Letter {
-    t0: number;
-    t1: number;
-    pitch: number;
-    syllable: string;
-    isManualPitch: boolean;
-    isWordSep: boolean;
+  t0: number;
+  t1: number;
+  pitch: number;
+  syllable: string;
+  isManualPitch: boolean;
+  isWordSep: boolean;
 }
 
 export interface Speaker {
-    uploadId: string;
-    letters: Letter[];
-    fileIndex?: number;
-    latestAnalysisId?: number;
-    lastUploadedLetters?: Letter[];
-    latestAnalysisName?: string;
+  uploadId: string;
+  speakerName?: string;
+  letters: Letter[];
+  fileIndex?: number;
+  latestAnalysisId?: number;
+  lastUploadedLetters?: Letter[];
+  latestAnalysisName?: string;
 }
