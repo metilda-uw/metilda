09a63fa1c51344fc1a9e496c46583929d8227a93
psanjay5 <penmetsa@uw.edu>
2022-07-11 22:47:56 -0700

    added realtime db confing

--- frontend/src/Firebase/firebase.ts
+++ frontend/src/Firebase/firebase.ts
@@ -1,18 +1,18 @@
 import app from "firebase/app";
 import firebase from "firebase/app";
 import "firebase/auth";
-// import "firebase/database";
+import "firebase/database";
 import "firebase/firestore";
 import "firebase/storage";
-import "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyApNGjZ64mit03pD_92T7EZZ15UjeSxy44",
   authDomain: "metilda-development.firebaseapp.com",
+  databaseURL: "https://metilda-development-default-rtdb.firebaseio.com",
   projectId: "metilda-development",
   storageBucket: "metilda-development.appspot.com",
   messagingSenderId: "1093113246351",
-  appId: "1:1093113246351:web:38d5913355eb8fe32eb6fa",
+  appId: "1:1093113246351:web:38d5913355eb8fe32eb6fa"
 };
 
 class Firebase {
@@ -20,6 +20,7 @@ class Firebase {
   firestore: any;
   timestamp: any;
   storage: any;
+  realtimedb: any;
 
   constructor() {
     app.initializeApp(firebaseConfig);
@@ -27,9 +28,11 @@ class Firebase {
     this.storage = app.storage();
     this.firestore = firebase.firestore();
     this.timestamp = firebase.firestore.Timestamp;
+    this.realtimedb = firebase.database();
   }
 
   // *** Auth API ***
+
   doCreateUserWithEmailAndPassword = (email: string, password: string) =>
     this.auth.createUserWithEmailAndPassword(email, password);
 
@@ -45,6 +48,13 @@ class Firebase {
 
   // *** Cloud Storage API ***/
   uploadFile = () => this.storage.ref();
+
+  getCreatePageData = (path: string) => {
+    const data = this.realtimedb.ref(path);
+    data.on('value', (snapshot) => {
+      console.log(snapshot.val());
+    })
+  }
 }
 
 export default Firebase;
