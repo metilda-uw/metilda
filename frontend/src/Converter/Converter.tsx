9d2d9cbe930da1238ae6db9994953e44dc778498
greeshmaparimi <sree1159@uw.edu>
2023-03-05 09:40:24 -0800

    Added conversion logic for converter page

--- frontend/src/Converter/Converter.tsx
+++ frontend/src/Converter/Converter.tsx
@@ -1,25 +1,49 @@
-import React from "react";
+import React, {useState } from "react";
 import Header from "../Components/header/Header";
 
-class Converter extends React.Component{
-    render() {
-        return (
-            <div>
-                <Header></Header>
-                {/* <form className="form-converter"> */}
-                <div style={{ display: "flex", alignItems: "center" }}>
-                    <p style={{ marginRight: "10px"}}>Hz:</p>
-                    <input style={{ marginRight: "10px", width: "50%"}} className="form-converter-scale-Hz" name="scale-Hz" type="text" placeholder="Enter value" />
-                </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                    <p style={{marginRight: "10px"}}>MeT:</p>
-                    <input style={{marginRight: "10px", width: "50%"}} className="form-collections-scale-MeT" name="scale-MeT" type="text" placeholder="Enter value"/>
-                </div>
+function Converter(){
+    const [hzValue, setHzValue] = useState(0);
+    const [MeTValue, setMeTValue] = useState(0);
 
-                    
-                {/* </form> */}
-            </div>    
-        );
-    }
+    const changeHzValue = (event : any) => {
+        setHzValue(event.target.value);
+        const constant = Math.pow(2,(1/48))
+        const MetVal = 440 * Math.pow(constant,event.target.value)
+        setMeTValue(MetVal);
+    };
+    const changeMeTValue = (event : any) => {
+        setMeTValue(event.target.value);
+        const HzVal = 48 * Math.log2((event.target.value/440));
+        setHzValue(HzVal);     
+    };
+
+    return (
+        <div>
+            <Header></Header>
+            {/* <div style={{ display: "grid", gridTemplateAreas:"auto"}}></div> */}
+            <div style={{ display: "flex", alignItems: "center",flexDirection:"row"}}>
+                <p style={{ marginRight: "10px"}}>Hz:</p>
+                <input style={{ marginRight: "10px", width: "50%"}} 
+                    className="converter-scale-Hz" 
+                    name="scale-Hz" 
+                    type="number" 
+                    value={hzValue}
+                    onChange={changeHzValue}
+                    placeholder="Enter value"
+                />
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <p style={{marginRight: "10px"}}>MeT:</p>
+                <input style={{marginRight: "10px", width: "50%"}} className="converter-scale-MeT" 
+                    name="scale-MeT" 
+                    type="number" 
+                    value={MeTValue}
+                    onChange={changeMeTValue}
+                    placeholder="Enter value"
+                />
+            </div>
+        </div>    
+    );
+
 }
 export default Converter;
