25bc048c114128fa91efbb9d7213d3e2eb6c9885
Ian Jones <anitahaircutnow@gmail.com>
2025-04-18 13:09:32 -0700

    Implemented dynamic question fetching
    Dropdown list now will show all questions from the database, could be
    changed on only active questions at a later time

--- frontend/src/Feedback/AdminAnalysis.tsx
+++ frontend/src/Feedback/AdminAnalysis.tsx
@@ -12,45 +12,54 @@ import { Chart as ChartJS, Title, BarElement, Tooltip, Legend } from 'chart.js'
 import { Bar } from 'react-chartjs-2'
 
 const AdminAnalysis: React.FC = () => {
-  const [selectedQuestion, setSelectedQuestion] = useState<string>('')
+  const [selectedQuestion, setSelectedQuestion] = useState<number>()
   const [questionList, setQuestionList] = useState<dropdownQuestion[]>(null)
-  const [answerLabels, setAnswerLabels] = useState<string[]>([])
+  const [answerLabels, setAnswerLabels] = useState<string[]>(null)
+  const [answerData, setAnswerData] = useState<number[]>([])
 
   const handleQuestionSelect = (event) => {
     // see MUI v4 docs for explanation of how the select event value 
     // gets passed and is handled here:
     // https://v4.mui.com/api/select/
-    setSelectedQuestion(event.target.value as string)
+    setSelectedQuestion(event.target.value as number)
   }
 
-  // will use API to dynamically gather these in full implementation.
-  // const answerLabels = [
-  //   'Strongly disagree',
-  //   'Somewhat disagree',
-  //   'Neither agree nor disagree',
-  //   'Somewhat agree',
-  //   'Strongly agree',
-  //   'Not applicable'
-  // ]
-
+  // dynamically create answer labels
   const fetchAnswerLabels = async () => {
     const options = await axios.get('/api/getOptions')
     console.log(options)
-    //const processed = JSON.parse(options.data.result)
-    // processed.map((processed) => {
-    //   processed. as string; // get the name of the option
-    // })
     if (options.data.result) {
       const resultArr = options.data.result
       let optStrings: string[] = []
       resultArr.forEach((arr) => {
         optStrings.push(arr[1]) // grab string from array
       })
-      //console.log(processed)
       setAnswerLabels(optStrings)
     } else {
       NotificationManager.error('No comments found')
     }
+  }
+
+  // gets all questions and stores them as dropdown questions.
+  const fetchQuestions = async () => {
+    // whether it will be getQuestions or getAllQuestions is a design 
+    // choice that hasn't been made yet.
+    const questions = await axios.get('/api/getQuestions')
+    console.log(questions)
+    if (questions.data.result) {
+      const resultArr = questions.data.result
+      let ddQuestions: dropdownQuestion[] = []
+      resultArr.forEach((arr) => {
+        ddQuestions.push({ qName: arr[1], qid: arr[0] }) // grab string from array
+      })
+      setQuestionList(ddQuestions)
+    } else {
+      NotificationManager.error('No questions found')
+    }
+  }
+
+  const fetchAnswerCounts = async () => {
+    // need to implement API endpoint
   }
 
   // would be dynamically created depending on selectedQuestion in actual 
@@ -58,8 +67,9 @@ const AdminAnalysis: React.FC = () => {
   let chartData = {
     labels: answerLabels,
     datasets: [{
-      label: "Test data",
+      label: "Data for selected question",
       data: [1, 2, 3, 4, 5, 6]
+      // data: answerData
     }]
   }
 
@@ -83,46 +93,20 @@ const AdminAnalysis: React.FC = () => {
     return <Bar options={answerChartOptions} data={chartData}></Bar>
   }
 
-  const fetchFeedbackAnswers = async () => {
-    // can't implement this yet,
-    // focus on defining and establishing
-    // frontend design/static elements first
-    const answers = 'Not implemented yet!'
-    // whether it will be getQuestions or getAllQuestions is a design 
-    // choice that hasn't been made yet.
-    //const questions = await axios.get('/api/getQuestions')
-    console.log(answers)
-  }
-
   interface dropdownQuestion {
     qName: string
     qid: number
   }
 
-  // will get JSON using an API call which will then be converted 
-  // to an array of dropdownQuestions in the full implementation,
-  // temporarily just using this limited test data.
-  const fetchFeedbackQuestions = async () => {
-    const testQuestionList: { qName: string, qid: number }[] = [
-      { qName: "q1", qid: 1 },
-      { qName: "q2", qid: 2 },
-      { qName: "q3", qid: 3 }
-    ]
-    const testJSON: string = JSON.stringify(testQuestionList)
-    const qList: dropdownQuestion[] = JSON.parse(testJSON) as dropdownQuestion[]
-    if (qList !== null) {
-      console.log('qList:')
-      console.log(qList)
-    }
-    setQuestionList(qList)
-  }
-
   useEffect(() => {
-    fetchFeedbackQuestions()
+    fetchQuestions()
     fetchAnswerLabels()
   }, [])
 
-  if (questionList === null) {
+  if (questionList === null ||
+    answerLabels === null ||
+    answerData === null) {
+    console.log("something is null! loading...")
     return <div>Loading...</div>
   }
 
