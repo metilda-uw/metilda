608df5e637cf38eff5be67bc8274f59cc479fb41
Ian Jones <anitahaircutnow@gmail.com>
2025-04-19 12:38:05 -0700

    implemented success and error notifications for comments, label, and answer counts fetches

--- frontend/src/Feedback/AdminAnalysis.tsx
+++ frontend/src/Feedback/AdminAnalysis.tsx
@@ -16,6 +16,11 @@ const AdminAnalysis: React.FC = () => {
   const [answerLabels, setAnswerLabels] = useState<string[]>(null)
   const [answerData, setAnswerData] = useState<number[]>([])
 
+  interface dropdownQuestion {
+    qName: string
+    qid: number
+  }
+
   const handleQuestionSelect = (event) => {
     // see MUI v4 docs for explanation of how the select event value 
     // gets passed and is handled here:
@@ -25,17 +30,21 @@ const AdminAnalysis: React.FC = () => {
 
   // dynamically create answer labels
   const fetchAnswerLabels = async () => {
-    const options = await axios.get('/api/getOptions')
-    console.log(options)
-    if (options.data.result) {
-      const resultArr = options.data.result
-      let optStrings: string[] = []
-      resultArr.forEach((arr) => {
-        optStrings.push(arr[1].charAt(0).toUpperCase() + arr[1].slice(1)) // grab string from array + capitalize
-      })
-      setAnswerLabels(optStrings)
-    } else {
-      NotificationManager.error('No comments found')
+    try {
+      const options = await axios.get('/api/getOptions')
+      if (options.data.result) {
+        const resultArr = options.data.result
+        let optStrings: string[] = []
+        resultArr.forEach((arr) => {
+          optStrings.push(arr[1].charAt(0).toUpperCase() + arr[1].slice(1)) // grab string from array + capitalize
+        })
+        setAnswerLabels(optStrings)
+        NotificationManager.success('Answer Labels fetched successfully.');
+      } else {
+        NotificationManager.error('No labels found')
+      }
+    } catch (error) {
+      NotificationManager.error('Failed to fetch label list')
     }
   }
 
@@ -43,31 +52,39 @@ const AdminAnalysis: React.FC = () => {
   const fetchQuestions = async () => {
     // whether it will be getQuestions or getAllQuestions is a design 
     // choice that hasn't been made yet.
-    const questions = await axios.get('/api/getQuestions')
-    console.log(questions)
-    if (questions.data.result) {
-      const resultArr = questions.data.result
-      let ddQuestions: dropdownQuestion[] = []
-      resultArr.forEach((arr) => {
-        ddQuestions.push({ qName: arr[1], qid: arr[0] }) // grab string from array
-      })
-      setQuestionList(ddQuestions)
-    } else {
-      NotificationManager.error('No questions found')
+    try {
+      const questions = await axios.get('/api/getQuestions')
+      if (questions.data.result) {
+        const resultArr = questions.data.result
+        let ddQuestions: dropdownQuestion[] = []
+        resultArr.forEach((arr) => {
+          ddQuestions.push({ qName: arr[1], qid: arr[0] }) // grab string from array
+        })
+        setQuestionList(ddQuestions)
+        NotificationManager.success('Question list fetched successfully.');
+      } else {
+        NotificationManager.error('No questions found')
+      }
+    } catch (error) {
+      NotificationManager.error("Failed to fetch question list")
     }
   }
 
   // Fetch the number of answers for each option
   const fetchAnswerCounts = async () => {
-    const counts = await axios.get(`/api/getAnswerCounts/${selectedQuestion}`)
-    console.log(counts)
-    if (counts.data.result) {
-      const resultArr = counts.data.result
-      let countArr: number[] = []
-      resultArr.forEach((arr) => {
-        countArr.push(arr[1])
-      })
-      setAnswerData(countArr)
+    try {
+      const counts = await axios.get(`/api/getAnswerCounts/${selectedQuestion}`)
+      if (counts.data.result) {
+        const resultArr = counts.data.result
+        let countArr: number[] = []
+        resultArr.forEach((arr) => {
+          countArr.push(arr[1])
+        })
+        setAnswerData(countArr)
+        NotificationManager.success('Answer counts fetched successfully.');
+      }
+    } catch (error) {
+      NotificationManager.error("Failed to fetch answer counts")
     }
   }
 
@@ -118,14 +135,8 @@ const AdminAnalysis: React.FC = () => {
     }
   }
 
-  //const generateQuestionBarGraph = async () => {
   const generateQuestionBarGraph = () => {
     return <Bar options={answerChartOptions} data={chartData}></Bar>
-  }
-
-  interface dropdownQuestion {
-    qName: string
-    qid: number
   }
 
   // useEffect for initial page setup
@@ -145,7 +156,6 @@ const AdminAnalysis: React.FC = () => {
   if (questionList === null ||
     answerLabels === null ||
     answerData === null) {
-    console.log("something is null! loading...")
     return <div>Loading...</div>
   }
 
@@ -169,7 +179,6 @@ const AdminAnalysis: React.FC = () => {
               ))}
             </Select>
           </FormControl>
-
         </Box>
       </Box>
       {generateQuestionBarGraph()}
